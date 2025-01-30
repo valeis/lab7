65752e382b46872904df9a1377a938a467878702
valeis <vb23745@gmail.com>
2025-01-31 01:16:42 +0200

    added migrations

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddStackExchangeRedisCache(options =>
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
-        policy => policy.WithOrigins("http://localhost:3001") 
+        policy => policy.WithOrigins("http://localhost:3000") 
                         .AllowAnyMethod()
                         .AllowAnyHeader() 
                         .AllowCredentials()); 
